da1c32f2b8d15e2ff720b9a60121eff635fdb9e3
hanaarena <hanaarena@gmail.com>
2025-07-01 16:25:16 +0900

    feat(cron): add moji_3 cron job which response store on KV & run every 10 minute

--- src/bindings.d.ts
+++ src/bindings.d.ts
@@ -2,6 +2,7 @@ export interface Bindings {
   QUIZ_KV: KVNamespace;
   DB: D1Database;
   GEMINI_API_KEY: string;
+  WORKER_DOMAIN: string;
 }
 
 declare global {
---
+++ src/cron/index.ts
@@ -0,0 +1,41 @@
+import { Bindings } from "../bindings";
+import { generateRandomHash } from "../utils/string";
+
+const JOB_TYPE = "moji_3";
+
+export async function getMoji3(env: Bindings) {
+  console.log(`${JOB_TYPE} star cron job at`, new Date().toString());
+  return fetch(`${env.WORKER_DOMAIN}/api/quiz/gemini/questions`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      content: "hoolala",
+      name: JOB_TYPE
+    })
+  }).then(async (r) => {
+    const res = (await r.json()) as {
+      data: { generatedText: string; name: string };
+    };
+    // store generated text to KV
+    const { data } = res;
+    if (!data) {
+      console.error(`[${JOB_TYPE}] api failed`, new Date().toString());
+      return;
+    }
+
+    const { generatedText, name } = data;
+    if (!generatedText) {
+      console.error(`[${JOB_TYPE}] no generated text`, new Date().toString());
+      return;
+    }
+
+    await env.QUIZ_KV.put(
+      `${name}_${generateRandomHash(10)}`,
+      generatedText,
+      {
+        expirationTtl: 60 * 60 * 24 * 2
+      }
+    );
+    console.log(`[${JOB_TYPE}] cron job finished at`, new Date().toString());
+  });
+}
